9acf454a3dba6b065e762a2b775d9c816a860cdb
Deep Prajapati <dvprajapati7212@gmail.com>
2025-09-03 20:42:13 +0530

    feat: implemented search functionality and button functional

---
+++ script.js
@@ -0,0 +1,24 @@
+const api = "YOUR_API_KEY";
+
+const btn = document.querySelector(".search-btn");
+const ip=document.querySelector(".input-box")
+btn.addEventListener("click", () => {
+    let city = ip.value;
+    getWeather(city);
+})
+async function getWeather(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
+    const response = await fetch(url);
+    console.log(response);
+    
+    const data = await response.json();
+    
+    const temp = document.querySelector(".temprature");
+    temp.innerHTML = `${Math.floor(data.main.temp)}<sup>°C</sup>`;
+
+    const cityName = document.querySelector(".city-name");
+    cityName.textContent = data.name;
+    console.log(data);
+    
+}
+
